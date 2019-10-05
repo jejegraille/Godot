04e2bb19bccf38f1150ea6794d0ace78d387c32d
badfuret <jejegraille@gmail.com>
2019-10-05 18:04:53 +0200

    Create DR_setTimeOut.gd

---
+++ newNode/DR_setTimeOut.gd
@@ -0,0 +1,17 @@
+extends Node;
+
+class_name DR_setTimeOut
+
+var time : float;
+var callback : FuncRef;
+
+func _init(timeSeconde : float, node : Node, functionName : String):
+	time = timeSeconde;
+	callback = funcref(node, functionName);
+	node.add_child(self);
+
+func _process(delta):
+	time -= delta;
+	if time <= 0:
+		callback.call_func();
+		queue_free();
